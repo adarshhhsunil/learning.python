89f54f005317c902c91bd642a42a281d38ca867b
adarshhhsunil <adarshsunil322@gmail.com>
2020-08-14 19:52:37 +0530

    'while_loop'

---
+++ whileloop.py
@@ -0,0 +1,28 @@
+# i =1
+# num =int (input("number of lines"))
+# while i<=num:
+# 	  print('*'*i)
+# 	  i+=1
+
+
+
+# j=int (input("number of liness"))
+# while j>0:
+# 	  print('*'*j)
+# 	  j-=1
+
+
+
+i =1
+num =int (input("number of lines"))
+while i<=num:
+	print(' '*(num-i) +'*'*(i+1))
+	i+=1
+
+
+
+
+
+
+
+
