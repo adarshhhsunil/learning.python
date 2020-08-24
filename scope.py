45a02edc6237820f31a13ca7df2f77c75cfc9699
adarshhhsunil <adarshsunil322@gmail.com>
2020-08-24 20:27:23 +0530

    scope

--- scope.py
+++ scope.py
@@ -1,11 +1,14 @@
+global test
+test = "hello"
 def check_scope():
-	def do_local():
-		test="local test"
-	def do_non_local():
-		test="non local test"
-	def do_global():
-		test="global test"
-	test="default"
-	do_local()
-	print('the test value after do local : '+test)
+
+    def do_local():
+        test="local test"
+    def do_non_local():
+        test="non local test"
+    def do_global():
+        test="global test"
+    test="default"
+    do_local()
 check_scope()
+print('the test value after do local : '+test)
