1adcf2bdb17ac1d5d459dbae5a5d439833f18b60
adarshhhsunil <adarshsunil322@gmail.com>
2020-08-24 20:26:49 +0530

    class

---
+++ class.py
@@ -0,0 +1,20 @@
+class disturb:
+	def hello(self,n):
+		self.name=n
+		print(self.name)
+		
+
+		
+	def hai(self):
+		pass
+		
+
+
+x=disturb()
+y=disturb()
+name='ADARSH'
+x.hello(name)
+y.hello('SAM')
+
+
+
